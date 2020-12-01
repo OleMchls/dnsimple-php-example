91a2649beca5bfbba7d52ea2a3d10956a81f4cb4
Ole Michaelis <Ole.Michaelis@googlemail.com>
2020-12-01 13:35:15 +0100

    add dnsimple dependency

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
   require __DIR__ . '/vendor/autoload.php';
 
+  use Dnsimple\Client;
+
+  $client = new Client($_ENV["API_TOKEN"]);
+
+  $response = $client->identity->whoami();
+  $whoami = $response->getData();
+  $account_id = $whoami->account->id;
+
   echo "hello world!";
+  echo "Account ID: $account_id";
